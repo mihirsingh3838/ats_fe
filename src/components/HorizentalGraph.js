29f9c8aa5e1305b306e153677520f73da38e028d
ShivaniSharma <shivanisharma8092@gmail.com>
2024-07-30 16:27:25 +0530

    Updated user sidebar mobile view

--- src/components/HorizentalGraph.js
+++ src/components/HorizentalGraph.js
@@ -2,6 +2,8 @@
 import React from "react";
 import { FaHeart, FaHome, FaCamera, FaChartBar } from "react-icons/fa";
 import HorizentalGraphItem from "./HorizentalGraphItem";
+import { ImBriefcase } from "react-icons/im";
+import { HiClipboardList } from "react-icons/hi";
 
 const HorizentalGraph = () => {
   return (
@@ -15,36 +17,36 @@ const HorizentalGraph = () => {
           textColor="text-green-500"
           iconColor="bg-green-200"
           icon={<FaHeart />}
-          width="55"
+          width="100"
         />
         <HorizentalGraphItem
           title="Present"
           value="160"
           color="bg-gray-800"
           textColor="text-gray-800"
-          iconColor="bg-gray-400"
-          icon={<FaHome />}
-          width="50"
+          iconColor="bg-blue-200"
+          icon={<HiClipboardList />}
+          width="100"
         />
       </div>
       <div className="flex">
         <HorizentalGraphItem
-          title="Total Working Days"
+          title="Work Days"
           value="260"
           color="bg-gray-400"
-          textColor="text-gray-400"
+          textColor="text-gray-800"
           iconColor="bg-gray-200"
-          width="45"
-          icon={<FaCamera />}
+          width="100"
+          icon={<ImBriefcase />}
         />
         <HorizentalGraphItem
           title="Absent"
           value="4"
           color="bg-indigo-500"
           textColor="text-black-500"
-          iconColor="bg-blue-200"
+          iconColor="bg-red-400"
           icon={<FaChartBar />}
-          width="70"
+          width="100"
         />
       </div>
     </>
